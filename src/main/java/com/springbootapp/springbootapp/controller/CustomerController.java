3003d4cf45cbd2a0e65ce04c1da11f1c88a6bf5d
Pau <pauserratosa5@gmail.com>
2022-01-17 13:07:19 +0100

    Change import

--- src/main/java/com/springbootapp/springbootapp/controller/CustomerController.java
+++ src/main/java/com/springbootapp/springbootapp/controller/CustomerController.java
@@ -5,7 +5,6 @@ import com.springbootapp.springbootapp.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
