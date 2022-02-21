a2c4d1e8757d226c29ba51ca4a9ec9122c57e67b
Pau <pauserratosa5@gmail.com>
2022-02-21 18:27:24 +0100

    Change service to register and create

--- src/main/java/com/springbootapp/util/LoggerConstants.java
+++ src/main/java/com/springbootapp/util/LoggerConstants.java
@@ -1,6 +1,6 @@
 package com.springbootapp.util;
 
-import com.springbootapp.controller.CustomerController;
+import com.springbootapp.controller.CustomerRegisterController;
 import com.springbootapp.controller.ParkCreateController;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -11,6 +11,6 @@ import org.apache.commons.logging.LogFactory;
  */
 public class LoggerConstants {
 
-    public static final Log CustomerControllerLog = LogFactory.getLog(CustomerController.class);
+    public static final Log CustomerControllerLog = LogFactory.getLog(CustomerRegisterController.class);
     public static final Log ParkControllerLog = LogFactory.getLog(ParkCreateController.class);
 }
