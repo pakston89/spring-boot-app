f7ed99545a1b9990f3d3fd3eae1bc28c869db5f3
Pau <pauserratosa5@gmail.com>
2022-03-01 22:03:59 +0100

    Removing Log4j2 annotation in controllers

--- target/generated-sources/annotations/com/springbootapp/model/mapper/CustomerMapperImpl.java
+++ target/generated-sources/annotations/com/springbootapp/model/mapper/CustomerMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-03-01T21:57:36+0100",
+    date = "2022-03-01T22:03:35+0100",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.11 (Oracle Corporation)"
 )
 @Component
