b7dac8dcf991c8051676af2f5c2001ba3a7649ae
Pau <pauserratosa5@gmail.com>
2022-03-05 21:47:51 +0100

    Adding NotFoundException to CustomerServiceImpl.

--- src/main/java/com/springbootapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springbootapp/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.springbootapp.exception;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,10 +15,10 @@ public class GlobalExceptionHandler {
     private ErrorMessage errorMessage;
 
     @ExceptionHandler(value = EmptyResultDataAccessException.class)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ErrorMessage notFoundExceptionHandler(NotFoundException ex) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorMessage> notFoundExceptionHandler(NotFoundException ex) {
         errorMessage.setErrorCode("404");
         errorMessage.setMessage(ex.getMessage() + "eiii");
-        return errorMessage;
+        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
     }
 }
