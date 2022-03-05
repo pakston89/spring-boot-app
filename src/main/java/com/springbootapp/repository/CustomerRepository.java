32513b4743fe8d9aeb17a5050b98a768893c6279
Pau <pauserratosa5@gmail.com>
2022-03-05 20:57:22 +0100

    Error handling implementation

--- src/main/java/com/springbootapp/repository/CustomerRepository.java
+++ src/main/java/com/springbootapp/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.springbootapp.repository;
 
 import com.springbootapp.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
@@ -14,6 +15,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     @Query(value = "SELECT * FROM customers WHERE nif=:nif", nativeQuery = true)
     Customer getByNif(@Param("nif") String nif);
 
+    @Modifying
     @Query(value = "DELETE FROM customers WHERE id=:id", nativeQuery = true)
     void deleteCustomerById(@Param("id") Integer id);
 }
