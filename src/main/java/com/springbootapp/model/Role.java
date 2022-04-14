bf7a2e17d8597828f29080ce8fb37452b262b69e
Pau <pauserratosa5@gmail.com>
2022-04-14 12:43:55 +0200

    Password BCrypt hashing implementation.

---
+++ src/main/java/com/springbootapp/model/Role.java
@@ -0,0 +1,32 @@
+package com.springbootapp.model;
+
+import com.springbootapp.util.TableNames;
+import javax.persistence.*;
+
+@Entity(name = TableNames.ROLES)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/springbootapp/model/UserRole.java
@@ -0,0 +1,45 @@
+package com.springbootapp.model;
+
+import com.springbootapp.util.TableNames;
+import javax.persistence.*;
+
+@Entity(name = TableNames.USER_ROLE)
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
